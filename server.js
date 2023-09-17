79cc42e2ff8b1e26122a35c4f78bcc95607bb2a2
Darek Biernat <kaiju83@outlook.com>
2023-09-17 22:07:26 +0200

    Prepare project to publication, change server config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const db = require('./db');
+const path = require('path');
 
 // import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -12,14 +13,22 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api', testimonialsRoutes); // add testimonials routes to server
 app.use('/api', concertsRoutes); // add concertst routes to server
 app.use('/api', seatsRoutes); // add seats routes to server
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 
 app.use((req, res) => {
     res.status(404).json(db.messageStatus[1]);
   })
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-});
+  app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+  });
